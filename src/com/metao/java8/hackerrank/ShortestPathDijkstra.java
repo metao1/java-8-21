5ea4b2e5b4d957be6beec1aa1b4f8981aa06d1b0
Mehrdad Karami <mehrdad.karami@ippen-digital.de>
2021-12-23 05:32:11 +0100

    best programmer competition

--- src/com/metao/java8/hackerrank/ShortestPathDijkstra.java
+++ src/com/metao/java8/hackerrank/ShortestPathDijkstra.java
@@ -6,80 +6,165 @@ public class ShortestPathDijkstra {
 
     public static void main(String[] args) {
         int n = 6, m = 2, s = 1;
-        List<Graph.Node> graph = Graph.createGraph(n, m);
-        printShortestPath(n, m, graph, s);
+        final Graph graph = Graph.createGraph(n);
+        for (int i = 0; i < n; i++) {
+            graph.addVertex(new Vertex(i));
+        }
+        final List<Vertex> vertices = graph.vertices();
+        vertices.get(1).edges = List.of(new Graph.Edge(2, 6), new Graph.Edge(3, 6));
+
+        printShortestPath(n, graph, s);
+        //final List<Integer> shortestPath = getShortestPath(n, m, List.of(List.of(1, 2), List.of(2, 3)), 1);
+        //System.out.println(shortestPath);
     }
 
-    private static void printShortestPath(int n, int m, List<Graph.Node> graph, int s) {
-        if (graph.isEmpty()) {
-            return;
-        }
-        ArrayList<Boolean> seen = new ArrayList<>(n);
-        List<Integer> distance = new ArrayList<>(n);
-        List<Integer> parents = new ArrayList<>(n);
-        Queue<Integer> queue = new PriorityQueue<>();
-        queue.offer(s);//insert starting node into queue
+    private static List<Integer> getShortestPath(int n, int m, List<List<Integer>> edges, int s) {
+        List<Integer> distances = new ArrayList<>(n + 1);
+        List<Boolean> seen = new ArrayList<>();
         for (int i = 0; i < n + 1; i++) {
-            seen.add(false);
-            parents.add(-1);
-            distance.add(Integer.MAX_VALUE);
+            distances.add(-1);
+            seen.add(i, false);
         }
-        distance.set(s, 0);
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add(s);
         while (!queue.isEmpty()) {
-            int startIndex = queue.poll();
-            final Graph.Node node = graph.get(startIndex);
-            for (Graph.Vertex vertex : node.vertices) {
-                queue.add(vertex.v);
-            }
-            if (!seen.get(node.index)) {
-                seen.add(node.index, true);
-                for (Graph.Vertex vertex : node.vertices) {
-                    distance.set(vertex.v, Math.min(distance.get(node.index) + vertex.w, distance.get(vertex.v)));
+            Integer currentIndex = queue.poll();
+            if (!seen.get(currentIndex)) {
+                seen.set(currentIndex, true);
+                for (List<Integer> edge : edges) {
+                    if (edge.get(0).equals(currentIndex)) {
+                        queue.add(edge.get(1));
+                    }
+                }
+                for (Integer dest : queue) {
+                    int dist = distances.get(dest), current = distances.get(currentIndex);
+                    if (dist == -1) {
+                        dist = Integer.MAX_VALUE;
+                    }
+                    if (current == -1) {
+                        current = 0;
+                    }
+                    if (current + 6 < dist) {
+                        distances.set(dest, current + 6);
+                    }
                 }
             }
         }
-        distance = distance.subList(2, distance.size());
-        System.out.println(distance);
+        System.out.println(distances);
+        distances = distances.subList(s + 1, distances.size());
+        for (int i = 0; i < distances.size(); i++) {
+            if (distances.get(i) == Integer.MAX_VALUE) {
+                distances.set(i, -1);
+            }
+        }
+        return distances;
+    }
+
+    private static void printShortestPath(int n, Graph graph, int s) {
+        if (n == 0 || graph.isEmpty()) {
+            return;
+        }
+        final List<Vertex> vertices = graph.vertices();
+        List<Boolean> seen = new ArrayList<>(n + 1);
+        Queue<Vertex> queue = new PriorityQueue<>();
+        Vertex rootVertex = vertices.get(s);
+        queue.offer(rootVertex);//insert starting node into queue
+        for (int i = 0; i < n + 1; i++) {
+            seen.add(false);
+        }
+        rootVertex.cost = 0;
+        while (!queue.isEmpty()) {
+            Vertex pointedVertex = queue.poll();
+            final int pointedIndex = pointedVertex.index;
+            if (!seen.get(pointedIndex)) {
+                seen.set(pointedIndex, false);
+                final List<Graph.Edge> edges = pointedVertex.edges();
+                for (Graph.Edge edge : edges) {
+                    Vertex neighborVertex = vertices.get(edge.index);
+                    neighborVertex.setCost(Math.min(pointedVertex.cost + edge.w, neighborVertex.cost));
+                    queue.add(graph.vertices.get(edge.index));
+                }
+            }
+        }
+        System.out.println(graph.vertices);
     }
 
     private static class Graph {
-        private static List<Node> createGraph(int n, int m) {
-            List<Node> graph = new ArrayList<>(n + 1);
-            for (int i = 0; i < n + 1; i++) {
-                graph.add(new Node(i));
+        private static Graph instance = null;
+        private List<Vertex> vertices;
+
+        private Graph() {
+        }
+
+        public void addVertex(Vertex vertex) {
+            if (instance == null) {
+                return;
             }
-            addEdge(graph, 1, 2, 2);
-            addEdge(graph, 1, 3, 4);
-            addEdge(graph, 2, 3, 1);
-            addEdge(graph, 2, 4, 7);
-            addEdge(graph, 3, 5, 3);
-            addEdge(graph, 4, 6, 1);
-            addEdge(graph, 5, 4, 2);
-            addEdge(graph, 5, 6, 5);
-            return graph;
+            instance.vertices.add(vertex);
         }
 
-        private static void addEdge(List<Node> graph, int u, int v, int w) {
-            graph.get(u).vertices.add(new Vertex(v, w));
+        public List<Vertex> vertices() {
+            return instance.vertices;
         }
 
-        final static class Node {
-            int index;
-            List<Vertex> vertices = new ArrayList<>();
+        public static Graph createGraph(int n) {
+            if (instance == null) {
+                instance = new Graph();
+            }
+            instance.vertices = new ArrayList<>(n + 1);
+            for (int i = 1; i < n + 1; i++) {
+                instance.vertices.add(new Vertex(i));
+            }
+            return instance;
+        }
 
-            Node(int index) {
+        public boolean isEmpty() {
+            return instance.vertices == null;
+        }
+
+        public final static class Edge {
+            int index, w;
+
+            Edge(int index, int w) {
                 this.index = index;
-            }
-        }
-
-        final static class Vertex {
-            int w, v;
-
-            Vertex(int v, int w) {
-                this.v = v;
                 this.w = w;
             }
         }
-
     }
+
+    static final class Vertex implements Comparator<Vertex> {
+        int index;
+        List<Graph.Edge> edges = new ArrayList<>();
+        int cost = Integer.MAX_VALUE;
+
+        Vertex(int index) {
+            this.index = index;
+        }
+
+        public void addEdge(Graph.Edge edge) {
+            this.edges.add(edge);
+        }
+
+        private void setCost(int cost) {
+            this.cost = cost;
+        }
+
+        int getCost() {
+            return cost;
+        }
+
+        List<Graph.Edge> edges() {
+            return edges;
+        }
+
+        @Override
+        public int compare(Vertex v1, Vertex v2) {
+            if (v1.cost < v2.cost)
+                return -1;
+            else if (v1.cost > v2.cost)
+                return 1;
+            return 0;
+        }
+    }
+
 }
