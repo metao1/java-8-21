bb3599d6bd16d277989977ca82fe2a595ac9ea37
mehrdad.karami <mehrdadallahkarami@gmail.com>
2024-12-25 15:51:55 +0100

    updated readme

--- src/com/metao/examples/reactive/FlatMapIterableExample.java
+++ src/com/metao/examples/reactive/FlatMapIterableExample.java
@@ -1,114 +1,121 @@
-//package com.metao.java8.reactive;
-//
-//import reactor.core.CorePublisher;
-//import reactor.core.publisher.Flux;
-//import reactor.core.publisher.Mono;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.concurrent.Flow;
-//import java.util.function.Consumer;
-//import java.util.function.Function;
-//import java.util.stream.IntStream;
-//
-//public class FlatMapIterableExample {
-//
-//    public static void main(String[] args) {
-//
-//        String s = "string=12&temp=213".chars()
-//                .flatMap(ch -> ch == '&' ? "&amp".chars() : IntStream.of(ch))
-//                .collect(StringBuffer::new, StringBuffer::appendCodePoint, StringBuffer::append)
-//                .toString();
-//        String s1 = new String(new char[]{'1', '2', '3'});
-//        System.out.println(s1);
-//        Checker collect = "String".chars()
-//                .collect(Checker::new, Checker::appendCodePoint, Checker::check);
-//
-//
-//        new FlatMapIterableExample()
-//                .decode(Mono.just(new DataBufferProcessor()))
-//                .transform(new MessageToStringTransformer())
-//                .subscribe(System.out::println, new StringMessageErrorConsumer(), new StringMessageConsumerHandler());
-//    }
-//
-//    static class Checker {
-//
-//        void appendCodePoint(int i) {
-//
-//        }
-//
-//        public StringBuffer check(Checker sd) {
-//            return new StringBuffer(sd.toString());
-//        }
-//
-//        void check(Checker checker, Checker checker1) {
-//
-//        }
-//    }
-//
-//    interface TestInterface<T, U> {
-//    }
-//
-//
-//    private static class StringMessageConsumerHandler implements Runnable {
-//
-//        @Override
-//        public void run() {
-//            System.out.println("finished consuming all messages.");
-//        }
-//    }
-//
-//    private static class StringMessageErrorConsumer implements Consumer<Throwable> {
-//
-//        @Override
-//        public void config(Throwable throwable) {
-//            System.err.println(throwable.getMessage());
-//        }
-//    }
-//
-//    private static class StringMessageConsumer implements Consumer<String> {
-//
-//        @Override
-//        public void config(String s) {
-//            System.out.println(s);
-//        }
-//    }
-//
-//    private static class MessageToStringTransformer implements Function<Flux<Message>, Publisher<String>> {
-//
-//        private Publisher<String> transformToString(Message<String> message) {
-//            String payload = message.getPayload();
-//            return Flux.just(payload);
-//        }
-//
-//
-//        @Override
-//        public Flow.Publisher<String> apply(Flux<Message> messages) {
-//            return transformToString(null);
-//        }
-//    }
-//
-//    private static class MessageDecoderFunction implements Function<DataBuffer, Iterable<? extends Message>> {
-//
-//        @Override
-//        public Iterable<? extends Message> apply(DataBuffer input) {
-//            List<Message> messages = new ArrayList<>();
-//            if (input.isAvailable()) {
-//                input.readFromQueue().drainTo(messages);
-//            }
-//
-//            return messages;
-//        }
-//
-//        void terminate() {
-//
-//        }
-//    }
-//
-//    public Flux<Message> decode(Publisher<DataBuffer> inputStream) {
-//        MessageDecoderFunction decoderFunction = new MessageDecoderFunction();
-//        return Flux.from(inputStream)
-//                .flatMapIterable(decoderFunction)
-//                .doOnTerminate(decoderFunction::terminate);
-//    }
-//}
+package com.metao.java8.reactive;
+
+import com.metao.examples.reactive.DataBuffer;
+import com.metao.examples.reactive.DataBufferProcessor;
+import com.metao.examples.reactive.Message;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class FlatMapIterableExample {
+
+    public static void main(String[] args) {
+
+        String s = "string=12&temp=213".chars()
+                .flatMap(ch -> ch == '&' ? "&amp".chars() : IntStream.of(ch))
+                .collect(StringBuffer::new, StringBuffer::appendCodePoint, StringBuffer::append)
+                .toString();
+        String s1 = new String(new char[]{'1', '2', '3'});
+        System.out.println(s1);
+        Checker collect = "String".chars()
+                .collect(Checker::new, Checker::appendCodePoint, Checker::check);
+
+
+        new FlatMapIterableExample()
+                .decode(Mono.just(new DataBufferProcessor()))
+                .transform(new MessageToStringTransformer())
+                .subscribe(System.out::println, new StringMessageErrorConsumer(), new StringMessageConsumerHandler());
+    }
+
+    static class Checker {
+
+        void appendCodePoint(int i) {
+
+        }
+
+        public StringBuffer check(Checker sd) {
+            return new StringBuffer(sd.toString());
+        }
+
+        void check(Checker checker, Checker checker1) {
+
+        }
+    }
+
+    interface TestInterface<T, U> {
+    }
+
+
+    private static class StringMessageConsumerHandler implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("finished consuming all messages.");
+        }
+    }
+
+    private static class StringMessageErrorConsumer implements Consumer<Throwable> {
+
+        /**
+         * Performs this operation on the given argument.
+         *
+         * @param throwable the input argument
+         */
+        @Override
+        public void accept(Throwable throwable) {
+            System.err.println(throwable.getMessage());
+        }
+    }
+
+    private static class StringMessageConsumer implements Consumer<String> {
+
+        @Override
+        public void accept(String s) {
+            System.out.println(s);
+        }
+    }
+
+    private static class MessageToStringTransformer implements Function<Flux<Message>, Publisher<String>> {
+
+        private Publisher<String> transformToString(Message<String> message) {
+            String payload = message.getPayload();
+            return Flux.just(payload);
+        }
+
+
+        @Override
+        public Publisher<String> apply(Flux<Message> messages) {
+            return transformToString(messages.blockFirst());
+        }
+    }
+
+    private static class MessageDecoderFunction implements Function<DataBuffer, Iterable<? extends Message>> {
+
+        @Override
+        public Iterable<? extends Message> apply(DataBuffer input) {
+            List<Message> messages = new ArrayList<>();
+            if (input.isAvailable()) {
+                input.readFromQueue().drainTo(messages);
+            }
+
+            return messages;
+        }
+
+        void terminate() {
+
+        }
+    }
+
+    public Flux<Message> decode(Publisher<DataBuffer> inputStream) {
+        MessageDecoderFunction decoderFunction = new MessageDecoderFunction();
+        return Flux.from(inputStream)
+                .flatMapIterable(decoderFunction)
+                .doOnTerminate(decoderFunction::terminate);
+    }
+}
--- src/com/metao/examples/reactive/ReactiveBackgroundTest.java
+++ src/com/metao/examples/reactive/ReactiveBackgroundTest.java
@@ -1,27 +1,27 @@
-//package com.metao.java8.reactive;
-//
-//import reactor.core.publisher.Flux;
-//import reactor.core.scheduler.Schedulers;
-//
-//public class ReactiveBackgroundTest {
-//
-//    public static void main(String[] args) throws InterruptedException {
-//        subscribeOnWithPublishOnTest();
-//    }
-//
-//    public static void subscribeOnWithPublishOnTest() throws InterruptedException {
-//        Flux.range(1, 3)
-//                .map(v -> debug(v, "map1"))
-//                .publishOn(Schedulers.parallel())
-//                .map(v -> debug(v * 100, "map2"))
-//                .subscribeOn(Schedulers.elastic())
-//                .subscribe(w -> debug(w, "subscribe"));
-//
-//        Thread.sleep(5000);
-//    }
-//
-//    private static Integer debug(Integer v, String map1) {
-//        System.out.println(map1 + ":" + v);
-//        return v;
-//    }
-//}
+package com.metao.examples.reactive;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class ReactiveBackgroundTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        subscribeOnWithPublishOnTest();
+    }
+
+    public static void subscribeOnWithPublishOnTest() throws InterruptedException {
+        Flux.range(1, 3)
+                .map(v -> debug(v, "map1"))
+                .publishOn(Schedulers.parallel())
+                .map(v -> debug(v * 100, "map2"))
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(w -> debug(w, "subscribe"));
+
+        Thread.sleep(5000);
+    }
+
+    private static Integer debug(Integer v, String map1) {
+        System.out.println(map1 + ":" + v);
+        return v;
+    }
+}
